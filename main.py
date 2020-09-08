185ad6bcd0b8c91bdd4583dbc470bf5463997d83
Castro0o <andre@andrecastro>
2020-09-08 18:24:20 +0200

    query and printout loop in function

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
+from typing import Dict
 from classes import Query, SMWCategoryORProp, SMWImportOverview
 from cli_args import parser
 from log import logger
 args = parser.parse_args()
 
 
-if __name__ == '__main__':
+def query2page(resource_type: str, sparql_fn: str, format_: str,
+               source:str) -> Dict:
+    query = Query(resource_type=resource_type,
+                  sparql_fn=sparql_fn,
+                  format_=format_,
+                  source=source)
 
-    smw_import_overview = SMWImportOverview(ontology_ns='aeon')
-    # properties
-    query = Query(resource_type='property',
-                  sparql_fn='query_properties.rq',
-                  format_='ttl',
-                  source='aeon/aeon.ttl')
     for printout in query.return_printout():
         item = SMWCategoryORProp(resource_type=query.resource_type,
                                  item_=printout,
@@ -19,8 +19,7 @@ if __name__ == '__main__':
         if item.item_dict.get('smw_datatype') not in [None, '']:
             item.create_wiki_item()
             smw_import_overview.properties.append(
-                (item.subject_name,
-                 str(item.item_dict['smw_datatype'])))
+                (item.subject_name, str(item.item_dict['smw_datatype'])))
             print(item.item_dict)
             print(item.wikipage_content)
             if args.write is True:
@@ -30,50 +29,32 @@ if __name__ == '__main__':
             logger.warning(
                 msg=f'{item.wikipage_name} MISSING aeon:SMW_datatype val. '
                     f'Not imported to wiki')
+    last_item = item
+    return last_item
 
-    query = Query(resource_type='category',
-                  sparql_fn='query_classes.rq',
-                  format_='ttl',
-                  source='aeon/aeon.ttl')
-    for printout in query.return_printout():
-        logger.debug(f'SPARQL printout:{printout}')
-        item = SMWCategoryORProp(resource_type=query.resource_type,
-                                 item_=printout,
-                                 ontology_ns='aeon')
-        logger.debug(f'SMWCategoryORProp item:{item}')
 
-        if item.item_dict.get('smw_import_info'):
-            item.create_wiki_item()
-            smw_import_overview.categories.append(
-                (item.subject_name,
-                 'Category'))
-            if args.write is True:
-                logger.debug(msg=f'Wrote {item.wikipage_name} to wiki')
-                item.write_wikipage()
-            # print(f'*****************{item.wikipage_name}*****************\n'
-            #       f'{item.wikipage_content}\n*****************\n')
-        else:
-            logger.warning(
-                msg=f'{item.wikipage_name} MISSING aeon:SMW_import_info  val. '
-                    f'Not imported to wiki')
+if __name__ == '__main__':
+
+    smw_import_overview = SMWImportOverview(ontology_ns='aeon')
+    # # properties
+    query2page(resource_type='property',
+               sparql_fn='query_properties.rq',
+               format_='ttl',
+               source='aeon/aeon.ttl')
+
+    lastclass = query2page(resource_type='category',
+                           sparql_fn='query_classes.rq',
+                           format_='ttl',
+                           source='aeon/aeon.ttl')
+
     smw_import_overview.wikipage_name = \
-        f'Mediawiki:Smw_import_{item.ontology_ns}'
+        f'Mediawiki:Smw_import_{lastclass.ontology_ns}'
     smw_import_overview.ontology_name = 'Academic Event Ontology (AEON)'
-    smw_import_overview.iri = item.iri+'#'
+    smw_import_overview.iri = lastclass.iri+'#'
     smw_import_overview.ontology_url = 'http://ontology.tib.eu/aeon/'
     smw_import_overview.create_smw_import()
     smw_import_overview.write_wikipage()
 
-
-
-    # query = Query(resource_type='property',
-    #               sparql_fn='query_properties.rq',
-    #               format_='ttl',
-    #               source='aeon/aeon.ttl')
-    # for printout in query.return_printout():
-    #     item = SPARQLitem(resource_type=query.resource_type,
-    #                       item_=printout,
-    #                       ontology_ns='aeon')
 
 # subject = last_property['subject']
 # iri_base = str(subject.defrag())
