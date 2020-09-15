7faee7fc65da7b3156bb5e8a9210f8b0e724df76
Castro0o <andre@andrecastro>
2020-09-15 10:23:43 +0200

    file_utils

---
+++ file_utils.py
@@ -0,0 +1,55 @@
+import yaml
+import sys
+from pathlib import Path
+from typing import Dict
+
+
+def wikidetails_present():
+    wikidetails = Path(__file__).parent.parent / 'wikidetails.yml'
+    if not wikidetails.is_file():
+        print(f'Error: {wikidetails} file is essential to '
+              f'write to the wiki is missing. Use wikidetails.yml.template '
+              f'to create it.')
+        sys.exit(1)
+
+
+def relative_read_f(relativepath2f: str) -> str:
+    path_file = Path(__file__).parent / relativepath2f
+    with open(path_file, 'r') as f:
+        f_content = f.read()
+    return f_content
+
+
+def yaml2dict(path: str) -> Dict:
+    with open(path, 'r') as yaml_f:
+        yaml_content = yaml_f.read()
+        yaml_dict = yaml.safe_load(yaml_content)
+    return yaml_dict
+
+
+def dict2yaml(path: str, data: Dict):
+    with open(path, 'w') as yaml_f:
+        yaml.safe_dump(data=data, stream=yaml_f)
+
+
+def yaml_get_source(relativepath2f: str, method='read', data=None) -> Dict:
+    # print('CWD:', Path.cwd())
+    # print('parent:', Path(__file__).parent)
+    path_file = Path(__file__).parent / relativepath2f
+    if method == 'read':
+        yamldict = yaml2dict(path_file)
+        return yamldict
+    elif method == 'write':
+        dict2yaml(path=path_file, data=data)
+
+
+def yaml_get_mapping(mapping: str) -> Dict:
+    confid2ext_schema = yaml_get_source(f'../mapping2confident/{mapping}.yml')
+    # ext_schema = list(confid2ext_schema.keys())[0]
+    # ext_schema_mapping = confid2ext_schema[ext_schema]
+    return confid2ext_schema
+
+
+def createglobals(source='wikidata'):
+    confid_mapping.update(yaml_get_source(f'{source}/confident_mapping.yml'))
+    invert_confid_map.update(invert_mapping(schema=source))
