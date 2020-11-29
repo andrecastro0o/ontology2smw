287351bf34b115990f0b696e4dd992155969adcc
Castro0o <andre@andrecastro>
2020-11-29 11:02:01 +0100

    cli defaults

--- ontology2smw/cli_args.py
+++ ontology2smw/cli_args.py
@@ -5,15 +5,16 @@ parser = ArgumentParser(
                    ___    
                  { . . } 
 --------------o00---'----00o--
-confIDent ontology2SMW  
+              ontology2SMW  
 -----------------------------""", formatter_class=RawTextHelpFormatter)
 parser.add_argument('-w', '--write', action='store_true',
                     help="writes the output to wiki or file. "
                          "Default: False (dry-run).")
 parser.add_argument('-o', '--ontology',
-                    default='aeon/aeon.ttl',
-                    help="Ontology file or URI")
+                    default='https://raw.githubusercontent.com/tibonto/aeon/master/aeon.ttl',
+                    help="Ontology file or URI. "
+                    "Default: https://raw.githubusercontent.com/tibonto/aeon/master/aeon.ttl ")
 parser.add_argument('-f', '--format',
                     choices=['application/rdf+xml', 'ttl'],
                     default='ttl',
-                    help="Ontology format. Default value: application/rdf+xml")
+                    help="Ontology format. Default value: ttl")
